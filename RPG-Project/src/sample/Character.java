506ec1938dd2a412fa57b5b88e2a86b74df83494
dms224 <derek.steeg@usask.ca>
2022-03-09 21:30:26 -0600

    Added generateName() to Character.java Added generateStory() to Character.java

--- RPG-Project/src/sample/Character.java
+++ RPG-Project/src/sample/Character.java
@@ -3,16 +3,17 @@ package sample;
 public class Character {
     Stats characterStats;
     Features characterFeatures;
-    Story characterStory;
+    String characterStory;
     String name;
+    NamePool namePool;
 
     public Character() {
         // When a character class is created, each attribute is generated from a unique set of algorithms
         characterStats = generateStats();
         characterFeatures = generateFeatures();
+        namePool = new NamePool();
         name = generateName();
         characterStory = generateStory();
-
     }
 
     private Stats generateStats() {
@@ -25,11 +26,31 @@ public class Character {
     }
 
     private String generateName() {
-        return null;
+        return namePool.fetchName();
     }
 
-    private Story generateStory() {
-        return null;
+    /**
+     * generateStory()
+     *  The 'Story' of a character is just a description of their appearance.
+     *  It is represented as a String value, where each feature is noted.
+     *  The only things that changes in the Story String are the Character Features (generated randomly)
+     */
+    private String generateStory() {
+        characterStory = "You are a(n) "+characterFeatures.race+". ";
+        characterStory = characterStory.concat("Your hair is "+characterFeatures.hairType+" and noticeably "+characterFeatures.hairColor+". ");
+        characterStory = characterStory.concat("Your eyes shine a bold "+characterFeatures.eyeColor+" color. ");
+        characterStory =  characterStory.concat("Your build is "+characterFeatures.bodyType+"...and impressive. ");
+        characterStory = characterStory.concat("You are brimming with vitality, and ripe for adventure!");
+
+        return characterStory;
+    }
+
+
+    public static void main(String[] args) {
+
+        Character c = new Character();
+
+        System.out.println(c.characterStory);
     }
 
 }
