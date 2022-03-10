18061a83cb320066de3eb59b5f6ad3360bd7bf0a
Shaobo Xu (shx863) <shx863@mail.usask.ca>
2022-03-10 14:01:48 -0600

    update saveSystem

--- RPG-Project/src/sample/SaveSystem.java
+++ RPG-Project/src/sample/SaveSystem.java
@@ -4,85 +4,83 @@ package sample;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.List;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SaveSystem {
     /**
      *save the particular type of data to the file you want to name
-     * @param path the file name you want to set
-     * @param key list contains the name of the data you want to save
-     * @param value list contains the content you want to save
+     * @param Filename the file name you want to set
+     * @param character the character want to save
      * @throws FileNotFoundException
      */
-    Character character;
-    transData transData;
+    public static void SaveToFile(String Filename, Character character) throws FileNotFoundException {
+        transData data=new transData();
+        ArrayList<String> key=data.CreateKeyList();
+        ArrayList<String> value=data.CreateValueList(character);
 
-    public void SaveToFile(String path, ArrayList<String> key, ArrayList<String> value) throws FileNotFoundException {
         int length = 0;
-        File savefile = new File(path);
-        PrintWriter filenName = new PrintWriter(savefile);
+        File savefile = new File(Filename);
+        PrintWriter fileName = new PrintWriter(savefile);
 
-        while (length<=4){
-
-            filenName.println(key.get(length)+ " : ");
-            if(length==0){
-                filenName.println(value.get(0));
-            }
+        while (length<4){
+            fileName.println(key.get(length)+ " : ");
+            fileName.println("  ");
             if(length ==1){
-                int i=1;
-                while (i<=8){
-                    filenName.println(key.get(i+3));
-                    filenName.println(value.get(i));
+                int i=0;
+                while (i<=7){
+                    fileName.println(key.get(i+4)+" :");
+                    fileName.println(value.get(i));
                     i++;
                 }
             }
             if(length ==2){
                 int o =0;
                 while (o<=4){
-                    filenName.println(key.get(o+12));
-                    filenName.println(value.get(o+9));
+                    fileName.println(key.get(o+12)+" :");
+                    fileName.println(value.get(o+8));
                     o++;
                 }
             }
             length++;
-
         }
-        filenName.close();
+        fileName.close();
     }
 
     /**
      * load the content of the chose file
      * @param filename file name you want to load
-     * @param key list that would contain the data from file
-     * @param value list that would contain the data from file
+     * @param character the character would contain the data from the file
      * @throws FileNotFoundException
      */
-    public void LoadFile(String filename, ArrayList<String> key, ArrayList<String> value) throws FileNotFoundException {
+    public static void LoadFile(String filename, Character character) throws FileNotFoundException {
+        transData data =new transData();
+        ArrayList<String> value = data.CreateValueList(character);
+        ArrayList<String> key = data.CreateKeyList();
         File thefile = new File(filename);
         Scanner scan = new Scanner(thefile);
         int length = 0;
         int i=0;
-        while(length>=0){
-            key.set(i,scan.nextLine());
+        while(length<=3){
+            key.set(length,scan.nextLine());
+            scan.nextLine();
             if(length==1){
                 while(i<=7){
+                    scan.nextLine();
                     value.set(i,scan.nextLine());
                     i++;
                 }
             }
             if(length ==2){
                 while(i<=12){
+                    scan.nextLine();
                     value.set(i,scan.nextLine());
                     i++;
                 }
             }
-            value.set(i,scan.nextLine());
             length++;
-
         }
-        transData.SetValueFromList(value);
-
+        data.SetValueFromList(value,character);
     }
 }
 
