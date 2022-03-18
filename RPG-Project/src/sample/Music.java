98d80dddb464616c8995e816fab04a394f2ec202
Shaobo Xu (shx863) <shx863@mail.usask.ca>
2022-03-17 18:03:21 -0600

    Upload New File

---
+++ RPG-Project/src/sample/Music.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Music {
+    protected String path=".wav";
+    protected File file = new File(path);
+    protected Clip c;
+    public Music(String scene) {
+        scene=scene+".wav";
+        file = new File(scene);
+        System.out.println("success music"+scene );
+        }
+
+    protected void playMusic(File file) {
+        if (file.exists()) {
+            try {
+                AudioInputStream audio = AudioSystem.getAudioInputStream(file);
+                this.c = AudioSystem.getClip();
+                System.out.println("success music");
+                this.c.open(audio);
+                this.c.loop(Clip.LOOP_CONTINUOUSLY);
+            } catch (IOException e) {
+
+            } catch (UnsupportedAudioFileException e) {
+
+            } catch (LineUnavailableException e) {
+
+            }
+        }
+    }
+    protected void stopMusic(){
+        this.c.stop();
+    }
+}
